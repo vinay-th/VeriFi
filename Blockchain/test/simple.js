3f8a94420639098fc5d546ae831480c873e8a593
Niomi <nioomeee@gmail.com>
2025-02-27 21:34:42 +0530

    trying to fix it

--- Blockchain/test/simple.js
+++ Blockchain/test/simple.js
@@ -2,7 +2,8 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Simple Test", function () {
-    it("Should access ethers.utils", function () {
-        expect(ethers.utils).to.be.an("object");
+    it("Should access ethers.utils", async function () {
+        const hre = require("hardhat");
+        expect(hre.ethers.utils).to.be.an("object");
     });
 });
