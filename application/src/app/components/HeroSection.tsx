9e2cb2309fcced9a3d7df024a0984a34c1f86cb3
Vinay Thakor <vinay.th.24@gmail.com>
2025-02-27 18:39:54 +0530

    Add HeroSection component and update layout structure; remove unused SVG files

---
+++ application/src/app/components/HeroSection.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div>
+      <Button>Get Started</Button>
+    </div>
+  );
+};
+
+export default HeroSection;
