cbd798bf5bbd65b34e6873beab3559ece15a4dbf
Niomi <nioomeee@gmail.com>
2025-02-27 22:04:34 +0530

    it workeddd finallllyyyyyyyy

--- Blockchain/test/simple.js
+++ Blockchain/test/simple.js
@@ -1,10 +1,16 @@
 const { expect } = require("chai");
-const ethers = require("ethers");
+const { ethers } = require("hardhat");
 
 describe("Simple Test", function () {
-    it("Should access ethers.utils", async function () {
-        console.log("Ethers object:", ethers); // Debugging
-        console.log("Ethers utils: ", ethers.utils);
-        expect(ethers.utils).to.be.an("object");
-    });
+  it("Should deploy and test a contract", async function () {
+    const [owner] = await ethers.getSigners();
+
+    const SimpleContract = await ethers.getContractFactory("SimpleContract");
+    const simple = await SimpleContract.deploy(100); // Pass an initial value
+    await simple.waitForDeployment();
+
+    console.log("Contract deployed to:", await simple.getAddress());
+
+    expect(await simple.getValue()).to.equal(100); // Verify initial value
+  });
 });
