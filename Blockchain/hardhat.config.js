591c9eb117849ec5938a24354dd11546048688d1
Niomi <nioomeee@gmail.com>
2025-03-04 22:12:57 +0530

    added env variables

--- Blockchain/hardhat.config.js
+++ Blockchain/hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config(); // To load environment variables from a .env file
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_SEPOLIA_URL, // Infura Sepolia RPC URL from .env
+      accounts: [process.env.PRIVATE_KEY], // Your wallet's private key
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY, // Optional: For verifying contracts on Etherscan
+  },
 };
