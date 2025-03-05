02303bed6bd466fa8376bc56ed4e169fb83b1384
Vinay Thakor <vinay.th.24@gmail.com>
2025-03-05 14:00:42 +0530

    Refactor student dashboard layout and components; replace StudentNavbar and add StudentSidebar with navigation functionality

---
+++ application/src/app/components/student/StudentNavbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StudentNavbar = () => {
+  return (
+    <>
+      <div className="flex flex-row w-screen h-24 bg-[#4b495e] justify-between items-center p-5 font-Rubik text-3xl text-white font-bold leading-[3rem]">
+        Student Dashboard
+      </div>
+    </>
+  );
+};
+
+export default StudentNavbar;
--- application/src/app/student-dashboard/page.tsx
+++ application/src/app/student-dashboard/page.tsx
@@ -1,22 +1,117 @@
+'use client';
 import React from 'react';
-import StudentNavbar from '@/app/components/StudentNavbar';
-import getRole from '@/lib/get-role';
-import { redirect } from 'next/navigation';
-import StudentSidebar from '../components/StudentSidebar';
+import StudentNavbar from '@/app/components/student/StudentNavbar';
+import StudentSidebar from '../components/student/StudentSidebar';
+import { GoHome, GoShareAndroid } from 'react-icons/go';
+import { IoDocumentOutline } from 'react-icons/io5';
+import { VscGitPullRequestGoToChanges } from 'react-icons/vsc';
 
-const page = async () => {
-  const role = await getRole();
+const Page = () => {
+  const [selected, setSelected] = React.useState('Dashboard');
 
-  if (role !== 'student') {
-    redirect('/get-started');
-  }
+  const handleClick = (item: string) => {
+    setSelected(item);
+  };
 
   return (
     <div className="flex bg-[#49465F]">
-      <StudentSidebar />
+      <StudentSidebar>
+        <div className="col-1 flex flex-col">
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-col gap-4">
+              {selected === 'Dashboard' ? (
+                <div
+                  className="w-40 h-12 bg-[rgb(173,166,234)] rounded-lg cursor-pointer p-3 flex flex-row"
+                  onClick={() => handleClick('Dashboard')}
+                >
+                  <GoHome className="mt-[0.1rem] mr-[0.1rem]" size={20} />
+                  Dashboard
+                </div>
+              ) : (
+                <div
+                  className="w-40 h-12 rounded-lg cursor-pointer p-3 flex flex-row"
+                  onClick={() => handleClick('Dashboard')}
+                >
+                  <GoHome className="mt-[0.1rem] mr-[0.1rem]" size={20} />
+                  Dashboard
+                </div>
+              )}
+              {selected === 'Document' ? (
+                <div
+                  className="w-40 h-12 bg-[rgb(173,166,234)] rounded-lg cursor-pointer p-3  flex flex-row"
+                  onClick={() => handleClick('Document')}
+                >
+                  <IoDocumentOutline
+                    className="mt-[0.1rem] mr-[0.1rem]"
+                    size={20}
+                  />
+                  Document
+                </div>
+              ) : (
+                <div
+                  className="w-40 h-12 rounded-lg cursor-pointer p-3  flex flex-row"
+                  onClick={() => handleClick('Document')}
+                >
+                  <IoDocumentOutline
+                    className="mt-[0.1rem] mr-[0.1rem]"
+                    size={20}
+                  />
+                  Document
+                </div>
+              )}
+              {selected === 'Shares' ? (
+                <div
+                  className="w-40 h-12 bg-[rgb(173,166,234)] rounded-lg cursor-pointer p-3  flex flex-row"
+                  onClick={() => handleClick('Shares')}
+                >
+                  <GoShareAndroid
+                    className="mt-[0.1rem] mr-[0.1rem]"
+                    size={20}
+                  />
+                  Shares
+                </div>
+              ) : (
+                <div
+                  className="w-40 h-12 rounded-lg cursor-pointer p-3  flex flex-row"
+                  onClick={() => handleClick('Shares')}
+                >
+                  <GoShareAndroid
+                    className="mt-[0.1rem] mr-[0.1rem]"
+                    size={20}
+                  />
+                  Shares
+                </div>
+              )}
+              {selected === 'Requests' ? (
+                <div
+                  className="w-40 h-12 bg-[rgb(173,166,234)] rounded-lg cursor-pointer p-3  flex flex-row"
+                  onClick={() => handleClick('Requests')}
+                >
+                  <VscGitPullRequestGoToChanges
+                    className="mt-[0.1rem] mr-[0.1rem]"
+                    size={20}
+                  />
+                  Requests
+                </div>
+              ) : (
+                <div
+                  className="w-40 h-12 rounded-lg cursor-pointer p-3  flex flex-row"
+                  onClick={() => handleClick('Requests')}
+                >
+                  <VscGitPullRequestGoToChanges
+                    className="mt-[0.1rem] mr-[0.1rem]"
+                    size={20}
+                  />
+                  Requests
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </StudentSidebar>
       <StudentNavbar />
     </div>
   );
 };
 
-export default page;
+export default Page;
